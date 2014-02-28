c28bfb0ceb87158cecd9cc0a4eca82123669d08e
Harish Rathi <harish.rathi@algosystech.com>
2014-02-28 10:57:03 +0530

    Added UserInterface Project

---
+++ UserInterface/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+
+namespace Piotr.ProductList
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RegisterWebApiConfig(GlobalConfiguration.Configuration);
+        }
+
+        private static void RegisterWebApiConfig(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
