d479c7c469621b98c9d8320aa71e19097648997f
MahendraPawar <mahendra.pawar@algosystech.com>
2014-03-07 14:20:39 +0530

    CLSN-6 Create Query Layer
    Stakeholder, StakeHierarchy, StakePayment, StakeWorking query changes
    completed

--- ColloSys.QueryBuilder/StakeholderBuilder/HierarchyQueryBuilder.cs
+++ ColloSys.QueryBuilder/StakeholderBuilder/HierarchyQueryBuilder.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using ColloSys.DataLayer.Domain;
 using ColloSys.DataLayer.Infra.SessionMgr;
 using ColloSys.QueryBuilder.BaseTypes;
 using ColloSys.QueryBuilder.TransAttributes;
-using NHibernate;
 using NHibernate.Criterion;
+using NHibernate.Linq;
 using NHibernate.Transform;
 
 namespace ColloSys.QueryBuilder.StakeholderBuilder
@@ -18,6 +19,17 @@ namespace ColloSys.QueryBuilder.StakeholderBuilder
             return Session.QueryOver<StkhHierarchy>()
                           .Where(x => x.Hierarchy != "Developer" && x.Hierarchy != "External")
                           .List();
+        }
+
+        [Transaction]
+        public IEnumerable<StkhHierarchy> OnDesignationHierarchy(string designation, string hierarchy)
+        {
+            var session = SessionManager.GetCurrentSession();
+
+            var data = session.Query<StkhHierarchy>()
+                              .Where(x => x.Designation == designation && x.Hierarchy == hierarchy)
+                              .Select(x => x).ToList();
+            return data;
         }
 
         public override QueryOver<StkhHierarchy,StkhHierarchy> DefaultQuery()
