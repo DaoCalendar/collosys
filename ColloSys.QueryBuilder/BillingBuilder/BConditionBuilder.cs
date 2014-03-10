d29aedaa8a1e21502951a7ef0a1f87186c40ef51
MahendraPawar <mahendra.pawar@algosystech.com>
2014-03-10 14:35:26 +0530

    QueryBuilder : AllocatioBuilder, ClientDataBuilder and BillingBuilder Added

---
+++ ColloSys.QueryBuilder/BillingBuilder/BConditionBuilder.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColloSys.DataLayer.Domain;
+using ColloSys.QueryBuilder.BaseTypes;
+using NHibernate.Criterion;
+
+namespace ColloSys.QueryBuilder.BillingBuilder
+{
+    public class BConditionBuilder:QueryBuilder<BCondition>
+    {
+        public override QueryOver<BCondition, BCondition> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BillAdhocBuilder:QueryBuilder<BillAdhoc>
+    {
+        public override QueryOver<BillAdhoc, BillAdhoc> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BillAmountBuilder:QueryBuilder<BillAmount>
+    {
+        public override QueryOver<BillAmount, BillAmount> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BillDetailBuilder:QueryBuilder<BillDetail>
+    {
+        public override QueryOver<BillDetail, BillDetail> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BillingPolicyBuilder:QueryBuilder<BillingPolicy>
+    {
+        public override QueryOver<BillingPolicy, BillingPolicy> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BillingRelationBuilder:QueryBuilder<BillingRelation>
+    {
+        public override QueryOver<BillingRelation, BillingRelation> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BillingSubpolicyBuilder:QueryBuilder<BillingSubpolicy>
+    {
+        public override QueryOver<BillingSubpolicy, BillingSubpolicy> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BillStatusBuilder:QueryBuilder<BillStatus>
+    {
+        public override QueryOver<BillStatus, BillStatus> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BMatrixBuilder:QueryBuilder<BMatrix>
+    {
+        public override QueryOver<BMatrix, BMatrix> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BMatrixValueBuilder:QueryBuilder<BMatrixValue>
+    {
+        public override QueryOver<BMatrixValue, BMatrixValue> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ColloSys.QueryBuilder/ClientDataBuilder/CacsActivityBuilder.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColloSys.DataLayer.ClientData;
+using ColloSys.DataLayer.Domain;
+using ColloSys.DataLayer.SharedDomain;
+using ColloSys.QueryBuilder.BaseTypes;
+using NHibernate.Criterion;
+
+namespace ColloSys.QueryBuilder.ClientDataBuilder
+{
+    public class CacsActivityBuilder : QueryBuilder<CacsActivity>
+    {
+        public override QueryOver<CacsActivity, CacsActivity> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CLinerBuilder:QueryBuilder<CLiner>
+    {
+        public override QueryOver<CLiner, CLiner> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CUnbilledBuilder:QueryBuilder<CUnbilled>
+    {
+        public override QueryOver<CUnbilled, CUnbilled> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CWriteoffBuilder:QueryBuilder<CWriteoff>
+    {
+        public override QueryOver<CWriteoff, CWriteoff> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ELinerBuilder:QueryBuilder<ELiner>
+    {
+        public override QueryOver<ELiner, ELiner> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EWriteoffBuilder:QueryBuilder<EWriteoff>
+    {
+        public override QueryOver<EWriteoff, EWriteoff> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class InfoBuilder:QueryBuilder<Info>
+    {
+        public override QueryOver<Info, Info> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PaymentBuilder:QueryBuilder<Payment>
+    {
+        public override QueryOver<Payment, Payment> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RLinerBuilder:QueryBuilder<RLiner>
+    {
+        public override QueryOver<RLiner, RLiner> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RWriteoffBuilder:QueryBuilder<RWriteoff>
+    {
+        public override QueryOver<RWriteoff, RWriteoff> DefaultQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
