b6fef0a9dace0d0dd1cd213cdda7a82fcec12f1f
Harish Rathi <harish.rathi@algosystech.com>
2014-03-19 18:06:44 +0530

    dev menu doen

--- UserInterface/Developer/queryexecuter/ExecuteQueryApiController.cs
+++ UserInterface/Developer/queryexecuter/ExecuteQueryApiController.cs
@@ -1,51 +1,36 @@
 ﻿#region references
 
-using System;
-using System.Data;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using ColloSys.UserInterface.Shared.Attributes;
 using ColloSys.UserInterface.Areas.Generic.Models;
+using ColloSys.UserInterface.Shared.Attributes;
 
 #endregion
 
-namespace ColloSys.UserInterface.Areas.Generic.apiController
+namespace AngularUI.Developer.queryexecuter
 {
     public class ExecuteQueryApiController : ApiController
     {
         [HttpGet]
-        [HttpTransaction]
         public HttpResponseMessage CheckandExecute(string query)
         {
-            var checkQuery = query.Substring(0,3);
+            var checkQuery = query.Trim().Substring(0, 3);
             var result = new QueryResult();
             result.AddQuery(query);
-            try
+            switch (checkQuery.ToUpper())
             {
-                switch (checkQuery.ToUpper())
-                {
-                    case "SEL":
-                        var dataSelect = QueryExecuter.ExecuteNonScaler(query);
-                        return Request.CreateResponse(HttpStatusCode.Created, dataSelect);
+                case "SEL":
+                    var dataSelect = QueryExecuter.ExecuteSelect(query);
+                    return Request.CreateResponse(HttpStatusCode.OK, dataSelect);
 
-                    case "UPD":
-                    case "DEL":
-                    case "INS":
-                        var dataUpdate = QueryExecuter.ExecuteNonQueryUpdateDelete(query);
-                        return Request.CreateResponse(HttpStatusCode.Created, dataUpdate);
-                    default:
-                        return Request.CreateResponse(HttpStatusCode.Created, "QUERY INVALID");
-                }
-            }
-            catch (Exception ex)
-            {
-               var dataTable = new DataTable();
-                dataTable.Columns.Add("Exception");
-                var dr = dataTable.NewRow();
-                dr["Exception"] = ex;
-                dataTable.Rows.Add(dr);
-                return Request.CreateResponse(HttpStatusCode.Created, dataTable);
+                case "UPD":
+                case "DEL":
+                case "INS":
+                    var dataUpdate = QueryExecuter.ExecuteDataChange(query);
+                    return Request.CreateResponse(HttpStatusCode.OK, dataUpdate);
+                default:
+                    return Request.CreateResponse(HttpStatusCode.OK, "QUERY INVALID");
             }
         }
     }
--- UserInterface/Developer/queryexecuter/queryexecuter.js
+++ UserInterface/Developer/queryexecuter/queryexecuter.js
@@ -1,65 +1,109 @@
 ﻿
-csapp.controller("queryExecuterController", ["$scope", "Restangular", "$csnotify", "$csfactory", function ($scope, rest, $csnotify, $csfactory) {
+csapp.factory("queryExecuterDataLayer", [
+    "Restangular", function (rest) {
 
-    var restApi = rest.all("ExecuteQueryApi");
-    $scope.gridOptions = {
-        data: 'tableData',
-        columnDefs: 'columnDefinition'
-    };
-    $scope.columnDefinition = [];
-    $scope.properties = [];
-    $scope.queryList = [];
-    $scope.executeQuery = function (data) {
-        if (angular.isDefined(data)) {
-            $scope.query = data;
+        var restApi = rest.all("ExecuteQueryApi");
+
+        var executeQuery = function (query2) {
+            return restApi.customGET("CheckandExecute", { query: query2 });
+        };
+
+        return {
+            executeQuery: executeQuery
+        };
+    }
+]);
+
+csapp.factory("queryExecuterFactory", ["queryExecuterDataLayer", "$csfactory", "lodash", function (datalayer, $csfactory, lodash) {
+
+    var addQueryToList = function (query, list) {
+        if ($csfactory.isNullOrEmptyString(query))
+            return list;
+        query = query.toLowerCase().trim();
+        if ($csfactory.isNullOrEmptyArray(list)) {
+            list = [];
+            list.push(query);
+            return list;
         }
-        if ($scope.query.length > 6) {
-            restApi.customGET("CheckandExecute", { query: $scope.query }).then(function (queryResult) {
-                debugger;
-                $scope.properties = [];
-                $scope.columnDefinition = [];
-                $scope.tableData = queryResult;
-                if (angular.isArray(queryResult)) {
-                    var obj = queryResult[0];
-                    //enlisting properties of an object
-                    for (var key in obj) {
-                        if (obj.hasOwnProperty(key) && typeof obj[key] !== 'function') {
-                            $scope.properties.push(key);
-                        }
-                    }
 
-                    //creating an array for columnDefs of ng-grid
-                    for (var i = 0; i < $scope.properties.length; i++) {
-                        var col = {};
-                        col.displayName = $scope.properties[i];
-                        col.width = 200;
-                        $scope.columnDefinition.push(col);
-                    }
-                }
-                $scope.queryType = $scope.query.substring(0, 3).toUpperCase();
+        var result = _.find(list, function (item) {
+            return item == query;
+        });
 
-                var dupQueryCheck = _.find($scope.queryList, function (item) {
-                    if (item === $scope.query)
-                        return item;
-                });
-                if (angular.isDefined(dupQueryCheck)) {
-                    $scope.queryList.splice($scope.queryList.indexOf($scope.query), 1);
-                    $scope.queryList.push($scope.query);
-                    $scope.queryList.reverse();
-                } else {
-                    $scope.queryList.push($scope.query);
-                    $scope.queryList.reverse();
-                }
+        if (angular.isUndefined(result)) {
+            list.push(query);
+        }
 
+        return list;
+    };
+
+    var getQueryType = function (query) {
+        return query.trim().substring(0, 3).toUpperCase();
+    };
+
+    var getProperties = function (queryResult) {
+        if ($csfactory.isNullOrEmptyArray(queryResult) || !angular.isArray(queryResult)) {
+            return [];
+        }
+
+        var obj = queryResult[0];
+        var properties = [];
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key) && typeof obj[key] !== 'function') {
+                properties.push(key);
+            }
+        }
+
+        return properties;
+    };
+
+    var getGridColumns = function (properties) {
+        var columnDefinition = [];
+        for (var i = 0; i < properties.length; i++) {
+            var col = {};
+            col.displayName = properties[i];
+            col.width = 200;
+            columnDefinition.push(col);
+            return columnDefinition;
+        }
+        return columnDefinition;
+    };
+
+    return {
+        addQueryToList: addQueryToList,
+        getQueryType: getQueryType,
+        getProperties: getProperties,
+        getGridColumns: getGridColumns
+    };
+}]);
+
+csapp.controller("queryExecuterController", ["$scope", "queryExecuterDataLayer", "queryExecuterFactory",
+    function ($scope, datalayer, factory) {
+
+        $scope.gridOptions = {
+            data: 'tableData',
+            columnDefs: 'columnDefinition'
+        };
+
+        $scope.executeQuery = function (query) {
+            if (query.length < 5) return;
+            $scope.tableData = [];
+
+            datalayer.executeQuery(query).then(function (queryResult) {
                 $scope.view = 'viewData';
-
+                $scope.queryList = factory.addQueryToList(query, $scope.queryList);
+                $scope.tableData = queryResult;
+                $scope.queryType = factory.getQueryType(query);
+                $scope.properties = factory.getProperties(queryResult);
+                $scope.columnDefinition = factory.getGridColumns($scope.properties);
+            }, function (error) {
+                $scope.view = 'viewError';
+                $scope.queryError = error;
             });
-        } else $csnotify.error("INVALID QUERY");
+        };
 
-
-    };
-    $scope.deleteQuery = function (data) {
-        $scope.queryList.splice($scope.queryList.indexOf(data), 1);
-    };
-}])
-;
+        $scope.deleteQuery = function (data) {
+            $scope.queryList.splice($scope.queryList.indexOf(data), 1);
+        };
+    }
+]);
