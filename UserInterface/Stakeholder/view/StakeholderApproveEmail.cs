9c34078d19d86a4dd1f2eb121b3597ba8dae0cb2
Sonu Autade <sonu.autade@algosystech.com>
2014-04-08 17:27:08 +0530

    CLSN-104  ViewStakeholder Screen : Stakeholder not getting save in 'G_User' Table, also mail not getting send to stakeholder and CLSN-50  forgot password not working

---
+++ UserInterface/Stakeholder/view/StakeholderApproveEmail.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using ColloSys.Shared.ConfigSectionReader;
+
+namespace EMailServices
+{
+    public class StakeholderApproveEmail
+    {
+        public static bool Send(string emailTo, string name, string userid)
+        {
+            try
+            {
+                var emailer = new Emailer(emailTo);
+                emailer.SetSubject("ColloSys - Access Granted");
+                emailer.SetBody(GetBodyContents(name, userid).ToString());
+                emailer.Send();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private static StringBuilder GetBodyContents(string userName, string userId)
+        {
+            var body = new StringBuilder();
+            body.Append("Dear  " + userName + ",");
+            body.AppendLine();
+            body.AppendLine();
+            body.Append("Your have been granted access to collosys.");
+            body.AppendLine();
+            body.Append("The access details are mention below:");
+            body.AppendLine();
+            body.AppendLine();
+            body.Append("UserID : " + userId);
+            body.AppendLine();
+            body.Append("Password : collosys");
+            body.AppendLine();
+            body.AppendLine();
+            body.AppendLine("Regards,");
+            body.AppendLine("ColloSys Support Team");
+            body.AppendLine();
+
+            return body;
+        }
+    }
+}
