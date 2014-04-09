0c9923a75a2898c32e2ad5da95f837f8574332b2
Harish Rathi <harish.rathi@algosystech.com>
2014-04-09 14:44:44 +0530

    CLSN-107 : get only counts

--- UserInterface/Generic/home/HomeApiController.cs
+++ UserInterface/Generic/home/HomeApiController.cs
@@ -1,11 +1,10 @@
-﻿using ColloSys.DataLayer.Allocation;
-using ColloSys.DataLayer.Domain;
+﻿using ColloSys.DataLayer.Domain;
 using ColloSys.DataLayer.Enumerations;
 using ColloSys.DataLayer.Infra.SessionMgr;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using NHibernate.Criterion;
 
 
 namespace AngularUI.Generic.home
@@ -13,23 +12,22 @@ namespace AngularUI.Generic.home
     public class HomeApiController : ApiController
     {
         [HttpGet]
-        
+
         public HttpResponseMessage GetData(string currentUser)
         {
             var id = currentUser;
             var session = SessionManager.GetCurrentSession();
             var data = new
             {
-                stakeholder = session.QueryOver<Stakeholders>().Where(x => x.ApprovedBy == id
-                    && x.Status == ColloSysEnums.ApproveStatus.Submitted).List().Count(),
-                working = session.QueryOver<StkhWorking>().Where(x => x.ApprovedBy == id
-                    && x.Status == ColloSysEnums.ApproveStatus.Submitted).List().Count(),
-                payment = session.QueryOver<StkhPayment>().Where(x => x.ApprovedBy == id
-                    && x.Status == ColloSysEnums.ApproveStatus.Submitted).List().Count(),
-                //allocation = session.QueryOver<AllocRelation>().Where(x => x.ApprovedBy == id
-                //    && x.Status == ColloSysEnums.ApproveStatus.Submitted).List().Count(),
-                //allocationpolicy = session.QueryOver<AllocRelation>().Where(x => x.ApprovedBy == id
-                //    && x.Status == ColloSysEnums.ApproveStatus.Submitted).List().Count()
+                stakeholder = session.QueryOver<Stakeholders>()
+                    .Where(x => x.ApprovedBy == id && x.Status == ColloSysEnums.ApproveStatus.Submitted)
+                    .Select(Projections.RowCount()).FutureValue<int>().Value,
+                working = session.QueryOver<StkhWorking>()
+                    .Where(x => x.ApprovedBy == id && x.Status == ColloSysEnums.ApproveStatus.Submitted)
+                    .Select(Projections.RowCount()).FutureValue<int>().Value,
+                payment = session.QueryOver<StkhPayment>()
+                    .Where(x => x.ApprovedBy == id && x.Status == ColloSysEnums.ApproveStatus.Submitted)
+                    .Select(Projections.RowCount()).FutureValue<int>().Value,
             };
             return Request.CreateResponse(HttpStatusCode.Created, data);
         }
