584d0c8ed7230b44887d7fd588f8b60ab29f458c
Sonu Autade <sonu.autade@algosystech.com>
2014-04-09 10:40:53 +0530

    CLSN-108  FileUpload :Create ApiController for fetching All Enums.

---
+++ UserInterface/Shared/webapis/SharedEnumsApiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ColloSys.DataLayer.Enumerations;
+
+namespace AngularUI.Shared.webapis
+{
+    public class SharedEnumsApiController : ApiController
+    {
+        //[HttpGet]
+        //public IEnumerable<string> GetEnums()
+        //{
+        //   return      
+        //}
+    }
+}
