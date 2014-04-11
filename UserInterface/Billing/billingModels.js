c1fc559d4ca30b156ae12699b64d89d5b8c76673
Mahendra Pawar <mahendra.pawar@algosystech.com>
2014-04-11 17:41:55 +0530

    adhoc bulk new screen changes

--- UserInterface/Billing/billingModels.js
+++ UserInterface/Billing/billingModels.js
@@ -3,13 +3,13 @@
 
     var billAdhoc = function () {
         return {
-            Stakeholder: { label: 'Hierarchy', type: 'text' },
+            Stakeholder: { label: 'Stakeholder', type: 'text', required: true },
             Products: { label: "Product", type: "enum", valueList: $csShared.enums.Products, required: true, },
             TotalAmount: { label: 'Total Amount', type: 'text', pattern: '/^[0-9]+$/', patternMessage: 'Please insert valid amount', required: true },
             IsRecurring: { label: 'IsRecurring', type: 'checkbox', },
             IsCredit: { label: 'Transaction Type', type: 'select', required: true },
             ReasonCode: { label: 'Reason', type: 'select', required: true, valueField: 'display', textField: 'display' },
-            StartMonth: { label: 'Start Month', type: 'select' },
+            StartMonth: { label: 'Start Month', type: 'select', required: true },
             Tenure: { label: 'Tenure', type: 'text', pattern: '/^[0-9]+$/', patternMessage: 'Tenure must be in 0-9' },
             Description: { label: 'Description', type: 'text', required: true }
         };
