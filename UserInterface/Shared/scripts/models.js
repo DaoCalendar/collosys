d32a9f10b634a3da7d7010fe919d1aece2d1a21d
Sonu Autade <sonu.autade@algosystech.com>
2014-04-11 15:10:03 +0530

    CLS-1403  Display of allocation screens

--- UserInterface/Shared/scripts/models.js
+++ UserInterface/Shared/scripts/models.js
@@ -123,19 +123,53 @@ csapp.factory("$csStakeholderModels", ["$csShared", function () {
     };
 
     return {
-        init: init()
+        init: init
     };
 
 }]);
+csapp.factory("$csAllocationModels", ["$csShared", function ($csShared) {
 
-csapp.factory("$csModels", ["$csFileUploadModels", "$csStakeholderModels",
-    function ($csFileUploadModels, $csStakeholderModels) {
+    var models = {};
+    var allocSubpolicy = function () {
+        return {
+            Stakeholder: { label: "Select Stakeholder", type: "" },//TOBE Disscuss list type
+            Name: { label: "Subpolicy Name", type: "text", pattern: "/^\w*$/", maxlength: 20, required: true },
+            AllocateType: { label: "Policy Allocate Type", type: "enum", valueList: $csShared.enums.AllocationType, required: true },
+            ReasonNotAllocate: { label: "Select Reason", type: "enums", valueList: "" },//TObe disscuss list
+            NoAllocMonth: { label: "Allocate Months", type: "int", min: 0, required: true },
+            Products: { label: "Product", type: "enum", valueList: $csShared.enums.Products, required: true },
+            Product: { label: "Product", type: "text", required: true, editable: false },
+            Category: { label: "Category", type: "enum", valueList: $csShared.enums.Category }
+        };
+    };
+
+    var allocPolicy = function () {
+        return {
+
+        };
+    };
+
+    var init = function () {
+        models.AllocSubpolicy = allocSubpolicy();
+        models.AllocPolicy = allocPolicy();
+        return models;
+    };
+    return {
+        init: init,
+        models: models
+    };
+}]);
+
+
+csapp.factory("$csModels", ["$csFileUploadModels", "$csStakeholderModels", "$csAllocationModels",
+    function ($csFileUploadModels, $csStakeholderModels, $csAllocationModels) {
 
         var models = {};
 
         var init = function () {
             models.FileUpload = $csFileUploadModels.init();
             models.Stakeholder = $csStakeholderModels.init;
+            models.AllocSubpolicy = $csAllocationModels.init();
             return;
         };
 
