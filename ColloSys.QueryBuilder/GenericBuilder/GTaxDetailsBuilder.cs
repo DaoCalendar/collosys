d1e68cb878af1118340f40af45655b0b997d272b
Mahendra Pawar <mahendra.pawar@algosystech.com>
2014-04-16 19:10:02 +0530

    CLS-1426 Step 12) Taxation : Setting up taxation computation. server side code for taxlist and taxmaster screen completed

---
+++ ColloSys.QueryBuilder/GenericBuilder/GTaxDetailsBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColloSys.DataLayer.Domain;
+using ColloSys.DataLayer.Infra.SessionMgr;
+using ColloSys.QueryBuilder.BaseTypes;
+using ColloSys.QueryBuilder.TransAttributes;
+using NHibernate.Linq;
+
+namespace ColloSys.QueryBuilder.GenericBuilder
+{
+    public class GTaxDetailsBuilder:Repository<GTaxDetail>
+    {
+        [Transaction]
+        public IEnumerable<GTaxDetail> GetAllWithRef()
+        {
+            return SessionManager.GetCurrentSession().QueryOver<GTaxDetail>()
+                                 .Fetch(x => x.GTaxesList).Eager.List();
+        }
+    }
+}
--- UserInterface/Generic/taxmaster/TaxMasterApiController.cs
+++ UserInterface/Generic/taxmaster/TaxMasterApiController.cs
@@ -4,18 +4,36 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using AngularUI.Shared.apis;
+using ColloSys.DataLayer.Domain;
 using ColloSys.QueryBuilder.GenericBuilder;
 
 namespace AngularUI.Generic.taxmaster
 {
-    public class TaxMasterApiController : ApiController
+    public class TaxMasterApiController : BaseApiController<GTaxDetail>
     {
         private static readonly GPincodeBuilder GPincodeBuilder = new GPincodeBuilder();
+        private static readonly GTaxesListBuilder GTaxesListBuilder=new GTaxesListBuilder();
+        private static readonly GTaxDetailsBuilder GTaxDetailsBuilder=new GTaxDetailsBuilder();
 
         [HttpGet]
-        public HttpResponseMessage StateList()
+        public HttpResponseMessage States()
         {
             var data = GPincodeBuilder.StateList();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GTaxList()
+        {
+            var data = GTaxesListBuilder.GetAll();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage TaxMasterList()
+        {
+            var data = GTaxDetailsBuilder.GetAllWithRef();
             return Request.CreateResponse(HttpStatusCode.OK, data);
         }
     }
