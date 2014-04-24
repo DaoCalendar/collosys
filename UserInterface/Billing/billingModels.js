2d9085395b3b38b651bb49bcd3aad7eced6cb2df
Mahendra Pawar <mahendra.pawar@algosystech.com>
2014-04-24 17:53:03 +0530

    CLSN-141 Create new screen for define holding policy and another for activate holding policy holding policy changes

--- UserInterface/Billing/billingModels.js
+++ UserInterface/Billing/billingModels.js
@@ -43,9 +43,9 @@
         };
     };
 
-    var formula = function() {
+    var formula = function () {
         return {
-            Name: {label:"Name",type:"text",required:true},
+            Name: { label: "Name", type: "text", required: true },
             Products: { label: "Product", type: "enum", valueList: $csShared.enums.Products },
             Description: { label: "Description", type: "textarea" },
             GroupBy: { label: "Group By", type: "select", required: true },
@@ -58,7 +58,7 @@
         };
     };
 
-    var matrix = function() {
+    var matrix = function () {
         return {
             Name: { label: "Name", type: "text", required: true },
             Dimension: { label: "Dimension", type: "select", },// to be disscuss
@@ -74,9 +74,34 @@
             Column4DCount: { label: "Columns 4D", type: "uint", min: 1, max: 10 },
             Column4DType: { type: "enum", valueList: $csShared.enums.PayoutLRType },
             Column4DTypeName: { type: "text" },
-            RowsOperator: { type: "enum", valueList: $csShared.enums.Operators},
+            RowsOperator: { type: "enum", valueList: $csShared.enums.Operators },
             ColumnsOperator: { type: "enum", valueList: $csShared.enums.Operators },
             MatrixPerType: { type: "enum", valueList: $csShared.enums.PayoutLRType },
+        };
+    };
+
+    var holdingPolicy = function () {
+        return {
+            Name: { label: 'Name', type: 'text', required: true },
+            Description: { label: 'Description', type: 'text', required: true },
+            StartDate: { label: 'Start Date', type: 'date', required: true },
+            EndDate: { label: 'End Date', type: 'date' },
+            ApplyOn: { label: 'Apply On', type: 'enum', valueList: $csShared.enums.ApplyOn, required: true },
+            Products: { label: "Product", type: "enum", valueList: $csShared.enums.ProductEnum, required: true, },
+            Rule: { label: "Rule", type: "enum", valueList: $csShared.enums.RuleForHolding, required: true, },
+            Value: { label: 'Value', type: 'number', required: true },
+            ValuePercent: { label: 'Value', type: 'number', template:'percentage', required: true },
+            TransactionType: { label: 'Transaction Type', type: 'radio', options: [{ value: 'Fixed', key: 'Fixed' }, { value: 'Recurring', key: 'Recurring' }], valueField: 'value', textField: 'key', required: true },
+            StartMonth: { label: 'Start Month', type: 'select', required: true, valueField: 'valuefield', textField: 'display' },
+            Tenure: { label: 'Tenure', type: 'number', max: 24, min: 1 },
+        };
+    };
+
+    var activateHolding = function () {
+        return {
+            HoldingPolicy: { label: 'Stakeholder', type: 'text', required: true },
+            Stakeholder: { label: 'Stakeholder', type: 'text', required: true },
+            Products: { label: "Product", type: "enum", valueList: $csShared.enums.ProductEnum, required: true, },
         };
     };
 
@@ -86,6 +111,8 @@
         models.BillingSubpolicy = billingSubpolicy();
         models.Formula = formula();
         models.Matrix = matrix();
+        models.HoldingPolicy = holdingPolicy();
+        models.ActivateHoldingPolicy = activateHolding();
         return models;
     };
 
--- UserInterface/Billing/holdingpolicy/holding-policy.js
+++ UserInterface/Billing/holdingpolicy/holding-policy.js
@@ -10,21 +10,97 @@
 csapp.factory('holdingpolicyDatalayer',
     ['Restangular', '$csnotify', '$csfactory',
         function (rest, $csnotify, $csfactory) {
-            var restApi = rest.all("PayoutPolicyApi");
+            var api = rest.all('HoldingPolicyApi');
             var dldata = {};
 
+            var create = function (policy) {
+                return api.post(policy).then(function (data) {
+                    $csnotify.success('data saved');
+                    return data;
+                });
+            };
+
+            var save = function (policy) {
+                return policy.put().then(function (data) {
+                    $csnotify.success('data saved');
+                    return data;
+                });
+            };
+            var getList = function () {
+                return api.getList().then(function (data) {
+                    return data;
+                });
+            };
             return {
                 dldata: dldata,
+                save: save,
+                create: create,
+                getList: getList
             };
         }]);
 
 csapp.controller('holdingpolicyCtrl', [
-    '$scope', 'holdingpolicyDatalayer', 'holdingpolicyFactory',
-    function ($scope, datalayer, factory) {
+    '$scope', 'holdingpolicyDatalayer', 'holdingpolicyFactory','$csModels',
+    function ($scope, datalayer, factory, $csModels) {
+
+        var calculateMonthList = function () {
+            var i = 1;
+                $scope.monthList = [];
+                for (var j = i; j < 6; j++) {
+                    var data = {
+                        valuefield: moment().add('month', j).format('YYYYMM'),
+                        display: moment().add('month', j).format("MMM-YYYY")
+                    };
+                    $scope.monthList.push(data);
+                }
+                $scope.HoldingPolicy.StartMonth.valueList = $scope.monthList;
+        };
+        
+        $scope.reset = function () {
+            $scope.policy = {};
+            $scope.holdingpolicyform.$setPristine();
+        };
+        var initLocal = function () {
+            $scope.PolicyList = $csModels.models.Billing.HoldingPolicy;
+            $scope.policyList = [];
+            $scope.policy = {};
+            $scope.indexOfSelected = -1;
+            $scope.isAddMode = true;
+            $scope.search = {};
+        };
+        
+        $scope.add = function (policy) {
+            //save tax then
+            datalayer.create(policy).then(function (data) {
+                $scope.policyList.push(data);
+                $scope.reset();
+            });
+        };
+
+        $scope.edit = function (policy, index) {
+            $scope.policy = angular.copy(policy);
+            $scope.indexOfSelected = index;
+            $scope.isAddMode = false;
+        };
+
+        $scope.applyedit = function (t) {
+            
+            datalayer.create(t).then(function (data) {
+                $scope.policyList[$scope.indexOfSelected] = data;
+                $scope.reset();
+                $scope.isAddMode = true;
+            });
+        };
 
         (function () {
             $scope.factory = factory;
             $scope.datalayer = datalayer;
             $scope.dldata = datalayer.dldata;
+            $scope.HoldingPolicy = $csModels.models.Billing.HoldingPolicy;
+            initLocal();
+            calculateMonthList();
+            datalayer.getList().then(function (data) {
+                $scope.policyList = data;
+            });
         })();
     }]);
