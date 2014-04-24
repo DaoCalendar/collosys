30302b0f7a2ffa147f24ab4b5377e4d914b3b990
Mahendra Pawar <mahendra.pawar@algosystech.com>
2014-04-24 16:30:40 +0530

    CLSN-141 Create new screen for define holding policy and another for activate holding policy api added for holding policy

--- ColloSys.QueryBuilder/BillingBuilder/HoldingPolicyBuilder.cs
+++ ColloSys.QueryBuilder/BillingBuilder/HoldingPolicyBuilder.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ColloSys.DataLayer.Mapping;
+﻿using ColloSys.DataLayer.Mapping;
 using ColloSys.QueryBuilder.BaseTypes;
 
 namespace ColloSys.QueryBuilder.BillingBuilder
