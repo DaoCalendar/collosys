692564e24adb956db7ed780271c7eb55c80fbf78
Sonu Autade <sonu.autade@algosystech.com>
2014-04-26 12:50:32 +0530

    CLS-1419 Step 4) Filter / Rejection Criteria

---
+++ UserInterface/FileUpload/filterCondition/filterCondition.js
@@ -0,0 +1,32 @@
+﻿//Datalayer
+csapp.factory("filterConditionDatalayer", ["Restangular", "$csnotify", function (rest, $csnotify) {
+
+    var restApi = rest.all("");
+    var dldata = {};
+    return {
+        dldata:dldata
+    };
+
+}]);
+
+//factory
+csapp.factory("filterconditionFactory", ["filterConditionDatalayer", function (datalayer) {
+    var dldata = datalayer.dldata;
+
+    return {
+        
+    };
+}]);
+
+//Controller
+csapp.controller("filterConditionController", ["$scope", "filterConditionDatalayer", "filterconditionFactory", "$csFileUploadModels",
+    function ($scope, datalayer, factory, $csFileUploadModels) {
+
+    (function () {
+        $scope.datalayer = datalayer;
+        $scope.dldata = datalayer.dldata;
+        $scope.factory = factory;
+        $scope.FilterCondition = $csFileUploadModels.models.FilterCondition();
+    })();
+
+}]);
