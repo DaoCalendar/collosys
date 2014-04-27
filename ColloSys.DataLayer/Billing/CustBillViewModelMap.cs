55cb65af812f2eb45c65e0c43f84f976536ff92f
Mayur Dongre <mayur.dongre@algosystech.com>
2014-04-27 14:39:09 +0530

    CLSN-39 : bill printing - excel generation - pdf Defect Billing Service

--- ColloSys.DataLayer/Billing/CustBillViewModelMap.cs
+++ ColloSys.DataLayer/Billing/CustBillViewModelMap.cs
@@ -14,28 +14,28 @@ namespace ColloSys.DataLayer.Mapping
     {
         public CustBillViewModelMap()
         {
-            Property( x=>x.AccountNo);
-            Property( x=>x.GlobalCustId);
-            Property( x=>x.Flag);
-            Property( x=>x.Product);
-            Property( x=>x.IsInRecovery);
-            Property(x=>x.ChargeofDate);
-            Property(x=>x.Cycle);
-            Property(x=>x.Bucket);
-            Property(x=>x.MobWriteoff);
-            Property(x=>x.MobWriteoff);
-            Property(x=>x.Vintage);
-            Property(x=>x.CityCategory);
+            Property(x => x.AccountNo);
+            Property(x => x.GlobalCustId);
+            Property(x => x.Flag);
+            Property(x => x.Product);
+            Property(x => x.IsInRecovery);
+            Property(x => x.ChargeofDate);
+            Property(x => x.Cycle);
+            Property(x => x.Bucket);
+            Property(x => x.MobWriteoff);
+            Property(x => x.MobWriteoff);
+            Property(x => x.Vintage);
+            Property(x => x.CityCategory);
             Property(x => x.City);
-            Property(x=>x.IsXHoldAccount);
-            Property(x=>x.AllocationStartDate);
-            Property(x=>x.AllocationEndDate);
-            Property(x=>x.TotalDueOnAllocation);
-            Property(x=>x.TotalAmountRecovered);
-            Property(x=>x.ResolutionPercentage);
-            Property(x=>x.ConditionSatisfy);
+            Property(x => x.IsXHoldAccount);
+            Property(x => x.AllocationStartDate);
+            Property(x => x.AllocationEndDate);
+            Property(x => x.TotalDueOnAllocation);
+            Property(x => x.TotalAmountRecovered);
+            Property(x => x.ResolutionPercentage);
+            Property(x => x.ConditionSatisfy, map => map.Length(4001));
 
-            ManyToOne(x=>x.BillDetail);
+            ManyToOne(x => x.BillDetail);
             ManyToOne(x => x.GPincode);
             ManyToOne(x => x.Stakeholders);
         }
--- UserInterface/Billing/summary/BillingAmountApiController.cs
+++ UserInterface/Billing/summary/BillingAmountApiController.cs
@@ -1,4 +1,11 @@
-﻿#region references
+﻿using System.IO;
+using AngularUI.Shared.webapis;
+using ColloSys.DataLayer.Billing;
+using ColloSys.Shared.SharedUtils;
+using NHibernate.Transform;
+using OfficeOpenXml;
+
+#region references
 
 using System;
 using System.Collections.Generic;
@@ -89,6 +96,178 @@ namespace ColloSys.UserInterface.Areas.Billing.apiController
             BillAmountBuilder.Save(finalBill.billAmount);
             return finalBill;
         }
+
+        // TODO : ICICI demo
+        #region ICICI demo
+        [HttpGet]
+        [HttpTransaction2(Persist = true)]
+        public string ExcelForBillSammary(ScbEnums.Products products, Guid stakeId, int month)
+        {
+            var billAmount = Session.QueryOver<BillAmount>()
+               .Where(x => x.Stakeholder.Id == stakeId)
+               .And(x => x.Products == products)
+               .And(x => x.Month == month)
+               .SingleOrDefault();
+
+            var billDetails = Session.QueryOver<BillDetail>()
+                                   .Fetch(x => x.BillingPolicy).Eager
+                                   .Fetch(x => x.BillingSubpolicy).Eager
+                                   .Fetch(x => x.PaymentSource).Eager
+                                   .Fetch(x => x.CustBillViewModels).Eager
+                                   .Where(x => x.Stakeholder.Id == stakeId)
+                                   .And(x => x.Products == products)
+                                   .And(x => x.BillMonth == month)
+                                   .And(x => x.PaymentSource == ColloSysEnums.PaymentSource.Variable)
+                                   .TransformUsing(Transformers.DistinctRootEntity)
+                                   .List();
+
+            if (billAmount == null)
+                return string.Empty;
+
+            var file = new FileInfo(Path.GetTempPath() + "\\BillSummary.xlsx");
+            var excelRowCounter = 1;
+            using (var pck = new ExcelPackage())
+            {
+                var ws = pck.Workbook.Worksheets.Add("StakeholderBillSummary");
+                WriteBillSammary(billAmount, ws, ref excelRowCounter);
+
+                foreach (var billDetail in billDetails)
+                {
+                    if (billDetail.Amount <= 0)
+                        continue;
+
+                    var wrCustBillViewModels = billDetail.CustBillViewModels.ToList();
+                    WriteBillDetails(billDetail, wrCustBillViewModels[0].ConditionSatisfy, ws, ref excelRowCounter);
+
+                    WriteCustBillViewModels(wrCustBillViewModels, ws, ref excelRowCounter);
+                }
+
+                pck.SaveAs(file);
+            }
+
+            return file.FullName;
+        }
+
+        private void WriteBillSammary(BillAmount billAmount, ExcelWorksheet ws, ref int excelRowCounter)
+        {
+            excelRowCounter++;
+
+            var startRow = excelRowCounter;
+
+            // stakeholder name
+            ws.Cells[excelRowCounter, 2].Value = "Stakeholder Name";
+            ws.Cells[excelRowCounter, 3].Value = billAmount.Stakeholder.Name;
+            excelRowCounter++;
+
+
+            // Month
+            ws.Cells[excelRowCounter, 2].Value = "Month";
+            ws.Cells[excelRowCounter, 3].Value = new DateTime((int)billAmount.Month / 100, (int)billAmount.Month % 100, 1).ToString("MMM-yyyy");
+            excelRowCounter++;
+
+            // Product
+            ws.Cells[excelRowCounter, 2].Value = "Product";
+            ws.Cells[excelRowCounter, 3].Value = billAmount.Products;
+            excelRowCounter++;
+
+            // Fixed Pay
+            ws.Cells[excelRowCounter, 2].Value = "Fixed Pay";
+            ws.Cells[excelRowCounter, 3].Value = billAmount.FixedAmount;
+            excelRowCounter++;
+
+            // Adhoc Pay
+            ws.Cells[excelRowCounter, 2].Value = "Adhoc Pay";
+            ws.Cells[excelRowCounter, 3].Value = billAmount.Deductions;
+            excelRowCounter++;
+
+            // Variable Pay
+            ws.Cells[excelRowCounter, 2].Value = "Variable Pay";
+            ws.Cells[excelRowCounter, 3].Value = billAmount.VariableAmount;
+            excelRowCounter++;
+
+            // Holding Payment
+            ws.Cells[excelRowCounter, 2].Value = "Holding Payment";
+            ws.Cells[excelRowCounter, 3].Value = billAmount.HoldAmount;
+            excelRowCounter++;
+
+            // Hol Release
+            ws.Cells[excelRowCounter, 2].Value = "Hold Release";
+            ws.Cells[excelRowCounter, 3].Value = billAmount.HoldRepayment;
+            excelRowCounter++;
+
+            // Variable Pay
+            ws.Cells[excelRowCounter, 2].Value = "Total Payout";
+            ws.Cells[excelRowCounter, 3].Value = billAmount.TotalAmount;
+            var endRow = excelRowCounter;
+            excelRowCounter++;
+
+            ws.Cells[startRow, 2, endRow, 2].Style.Font.Bold = true;
+        }
+
+        private void WriteBillDetails(BillDetail billDetail, string condition, ExcelWorksheet ws, ref int excelRowCounter)
+        {
+            excelRowCounter++;
+
+            var startRow = excelRowCounter;
+            // subpolicy name
+            ws.Cells[excelRowCounter, 2].Value = "Subpolicy Name";
+            ws.Cells[excelRowCounter, 3].Value = billDetail.BillingSubpolicy.Name;
+            excelRowCounter++;
+
+            // subpolicy amount
+            ws.Cells[excelRowCounter, 2].Value = "Subpolicy Amount";
+            ws.Cells[excelRowCounter, 3].Value = billDetail.Amount;
+            excelRowCounter++;
+
+            // subpolicy amount
+            ws.Cells[excelRowCounter, 2].Value = "Subpolicy Description";
+            ws.Cells[excelRowCounter, 3].Value = condition;
+            var endRow = excelRowCounter;
+            excelRowCounter++;
+
+            ws.Cells[startRow, 2, endRow, 2].Style.Font.Bold = true;
+        }
+
+        private void WriteCustBillViewModels(IEnumerable<ICustBillViewModel> listdata, ExcelWorksheet ws, ref int excelRowCounter)
+        {
+            //Create the worksheet
+
+            if (listdata == null)
+            {
+                ws.Cells[excelRowCounter, 2].Value = "No Data";
+                return;
+            }
+
+            // get properties to write
+            var entityAllPropsList = typeof(ICustBillViewModel).GetProperties();
+
+            //get our column headings
+            for (var i = 0; i < entityAllPropsList.Length; i++)
+            {
+                ws.Cells[excelRowCounter, i + 2].Value = entityAllPropsList[i].Name;
+            }
+
+            ws.Cells[excelRowCounter, 2, excelRowCounter, entityAllPropsList.Length + 1].Style.Font.Bold = true;
+            excelRowCounter++;
+
+            //populate our Data
+            foreach (var entity in listdata)
+            {
+                var propertyCounter = 2;
+                for (var i = 0; i < entityAllPropsList.Length; i++)
+                {
+                    if (entityAllPropsList[i].PropertyType == typeof(DateTime) || entityAllPropsList[i].PropertyType == typeof(DateTime?))
+                        ws.Cells[excelRowCounter, propertyCounter].Style.Numberformat.Format = "yyyy-mm-dd";
+
+                    ws.Cells[excelRowCounter, propertyCounter].Value
+                        = ReflectionUtil.GetPropertyValue(entity, entityAllPropsList[i].Name);
+                    propertyCounter = propertyCounter + 1;
+                }
+                excelRowCounter = excelRowCounter + 1;
+            }
+        }
+
+        #endregion
     }
 }
 
