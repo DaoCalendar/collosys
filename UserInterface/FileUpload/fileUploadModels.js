63e85eca3c81c9ff467c4d72c04a8045abfa29eb
Harish Rathi <harish.rathi@algosystech.com>
2014-05-09 20:13:32 +0530

    file scheduler done

--- UserInterface/FileUpload/fileUploadModels.js
+++ UserInterface/FileUpload/fileUploadModels.js
@@ -186,7 +186,7 @@
             SelectedDateWeekly: { label: 'File Date', type: 'date', template: 'Weekly' },
             SelectedDateMonthly: { label: 'File Date', type: 'date', template: 'Monthly' },
             IsImmediate: { label: 'Select Schedule', type: 'radio', options: [{ value: 'true', key: 'Immediate' }, { value: 'false', key: 'Nightly' }], valueField: 'value', textField: 'key' },
-            ImmediateReason: { label: 'ImmediateReason',type:'textarea' }
+            ImmediateReason: { label: 'ImmediateReason',type:'textarea', required: true, minlength: 5 }
         };
     };
 
