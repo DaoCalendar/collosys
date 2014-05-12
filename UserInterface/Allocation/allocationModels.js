beda80145655cc0a4c48218c0690dc93e4bae3e4
Sonu Autade <sonuautade@algosystech.com>
2014-05-12 11:31:51 +0530

    CLSN-214 Allocation : Convert Policy , SubPolicy and View/Approve js into model.js, also implement 'cs-field' into it's HTML.

--- UserInterface/Allocation/allocationModels.js
+++ UserInterface/Allocation/allocationModels.js
@@ -4,9 +4,14 @@
     var allocSubpolicy = function () {
         return {
             Stakeholder: { label: "Select Stakeholder", type: "" },//TOBE Disscuss list type
-            Name: { label: "Name", type: "text",  maxlength: 20, required: true },
-            AllocateType: { label: "Policy Allocate Type", type: "enum", valueList: $csShared.enums.AllocationType, required: true },
-            ReasonNotAllocate: { label: "Select Reason", type: "enum", valueList: "" },//TObe disscuss list
+            Name: { label: "Name", type: "text", maxlength: 20, required: true },
+            AllocateType: {
+                label: "Allocate Type", type: "select", valueField:"value", textField:"display",
+                valueList: [{ display: "Handle By Telecaller", value: "HandleByTelecaller" },
+        { display: "Do Not Allocate", value: "DoNotAllocate" },
+        { display: "Allocate As Per Stakeholder Working", value: "AllocateAsPerPolicy" },
+        { display: "Allocate to Particular Stakeholder", value: "AllocateToStkholder" }], required: true},
+            ReasonNotAllocate: { label: "Select Reason", type: "enum", valueList: [] },
             NoAllocMonth: { label: "Allocate Months", type: "number", template: 'int', min: 0, required: true },
             Products: { label: "Product", type: "enum", valueList: $csShared.enums.Products, required: true },
             ProductName: { label: "Product", type: "text", required: true, editable: false },
