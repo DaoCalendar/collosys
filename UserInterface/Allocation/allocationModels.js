904732f4047faa20ca5b93716329f298ee4a660c
Sonu Autade <sonuautade@algosystech.com>
2014-05-12 10:58:01 +0530

    CLSN-249 Allocation: Set the UI Properly

--- UserInterface/Allocation/allocationModels.js
+++ UserInterface/Allocation/allocationModels.js
@@ -4,12 +4,12 @@
     var allocSubpolicy = function () {
         return {
             Stakeholder: { label: "Select Stakeholder", type: "" },//TOBE Disscuss list type
-            Name: { label: "Subpolicy Name", type: "text", pattern: "/^\w*$/", maxlength: 20, required: true },
+            Name: { label: "Name", type: "text",  maxlength: 20, required: true },
             AllocateType: { label: "Policy Allocate Type", type: "enum", valueList: $csShared.enums.AllocationType, required: true },
             ReasonNotAllocate: { label: "Select Reason", type: "enum", valueList: "" },//TObe disscuss list
             NoAllocMonth: { label: "Allocate Months", type: "number", template: 'int', min: 0, required: true },
             Products: { label: "Product", type: "enum", valueList: $csShared.enums.Products, required: true },
-            Product: { label: "Product", type: "text", required: true, editable: false },
+            ProductName: { label: "Product", type: "text", required: true, editable: false },
             Category: { label: "Category", type: "enum", valueList: $csShared.enums.Category },
             CheckboxConditionOperators: { type: "enum", valueList: $csShared.enums.CheckboxConditionOperators, required: true },
             DropdownConditionOperators: { type: "enum", valueList: $csShared.enums.DropdownConditionOperators, required: true },
