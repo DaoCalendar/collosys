d2716a16563bf1bfb5a0d0c45e6b7086f823e4ed
Harish Rathi <harish.rathi@algosystech.com>
2014-05-20 18:17:52 +0530

    permission manager

--- ColloSys.QueryBuilder.Test/GenerateDb/Permission.cs
+++ ColloSys.QueryBuilder.Test/GenerateDb/Permission.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace ColloSys.QueryBuilder.Test.GenerateDb
@@ -9,43 +10,122 @@ namespace ColloSys.QueryBuilder.Test.GenerateDb
         [Test]
         public void CreatePermissions()
         {
-            var permission = new PermissionManager();
-
+            ActivityManager.CreateDevPermissions();
         }
     }
 
-    public class Permission
+    public class AppActivity
     {
-        public ActivityList Title { get; set; }
-        public string Link { get; set; }
-        public string Icon { get; set; }
+        // ReSharper disable MemberCanBePrivate.Global
+        // ReSharper disable UnusedAutoPropertyAccessor.Global
+        public ActivityList Name { get; set; }
         public string Description { get; set; }
         public bool HasAccess { get; set; }
-        public IList<Permission> Childrents { get; set; }
+        public IList<AppActivity> Childrens { get; set; }
+        // ReSharper restore UnusedAutoPropertyAccessor.Global
+        // ReSharper restore MemberCanBePrivate.Global
     }
 
-    public class PermissionManager
+    public static class ActivityManager
     {
-        private Permission Root { get; set; }
-        public PermissionManager()
+        private static void AddActivity(AppActivity parent, AppActivity child)
         {
-            Root = new Permission();
+            if (parent.Childrens == null)
+                parent.Childrens = new List<AppActivity>();
+            parent.Childrens.Add(child);
         }
 
-        public void AddPermission(Permission parent,  Permission child)
+        public static AppActivity AddActivity(AppActivity parent, ActivityList activity, string desciption="")
         {
-            if(parent.Childrents == null)
-                parent.Childrents = new List<Permission>();
-            parent.Childrents.Add(child);
+            var perm = new AppActivity
+            {
+                HasAccess = false,
+                Description = desciption,
+                Name = activity
+            };
+            AddActivity(parent, perm);
+            return perm;
+        }
+
+        public static bool CheckAccess(AppActivity root, IList<ActivityList> list)
+        {
+            var permission = root;
+            for (int i = 0; i < list.Count; i++)
+            {
+                var perm = permission.Childrens.FirstOrDefault(x => x.Name == list.ElementAt(i));
+                if (perm == null || perm.HasAccess == false) return false;
+                if (perm.Childrens == null || perm.Childrens.Count == 0) return true;
+                permission = perm;
+            }
+            return false;
+        }
+
+        public static AppActivity CreateDevPermissions()
+        {
+            var root = new AppActivity();
+
+            var fileupload = AddActivity(root, ActivityList.FileUploader);
+            AddFileUploadActivities(fileupload);
+
+            var stakeholder = AddActivity(root, ActivityList.Stakeholder);
+            AddStakeholderActivities(stakeholder);
+
+            var billing = AddActivity(root, ActivityList.Billing);
+            AddBillingActivities(billing);
+
+            var allocation = AddActivity(root, ActivityList.Allocation);
+            AddAllocationActivities(allocation);
+
+            var config = AddActivity(root, ActivityList.Config);
+            AddConfigActivities(config);
+
+            return root;
+        }
+
+        private static void AddFileUploadActivities(AppActivity root)
+        {
+            var createfile = AddActivity(root, ActivityList.CreateFile);
+            AddActivity(createfile, ActivityList.View);
+            AddActivity(createfile, ActivityList.AddEdit);
+            AddActivity(createfile, ActivityList.Approve);
+        }
+
+        private static void AddStakeholderActivities(AppActivity root)
+        {
+            AddActivity(root, ActivityList.CreateFile, "cofigure new file for upload");
+        }
+
+        private static void AddAllocationActivities(AppActivity root)
+        {
+            AddActivity(root, ActivityList.CreateFile, "cofigure new file for upload");
+        }
+
+        private static void AddBillingActivities(AppActivity root)
+        {
+            AddActivity(root, ActivityList.CreateFile, "cofigure new file for upload");
+        }
+
+        private static void AddConfigActivities(AppActivity root)
+        {
+            AddActivity(root, ActivityList.CreateFile, "cofigure new file for upload");
         }
     }
 
     public enum ActivityList
     {
+        AddEdit,
+        View,
+        Approve,
         FileUploader,
+        CreateFile,
+        ScheduleFile,
+        CustomerData,
+        UploadPincode,
+        ErrorCorrection,
         Stakeholder,
         Allocation,
         Billing,
-        Generic
+        Config,
+        AddHierarchy,
     }
 }
