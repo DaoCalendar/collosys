17af9a4264030bc8f5bbf5367fa97ae09ced8d21
Mayur Dongre <mayur.dongre@algosystech.com>
2014-05-30 17:44:48 +0530

    DHFL Billing service chages

---
+++ BillingService2/DBLayer/BillDetailDbLayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using ColloSys.DataLayer.ClientData;
+using ColloSys.DataLayer.Domain;
+using ColloSys.DataLayer.Infra.SessionMgr;
+using ColloSys.DataLayer.Billing;
+using ColloSys.DataLayer.SessionMgr;
+
+namespace BillingService2.DBLayer
+{
+    internal static class BillDetailDbLayer
+    {
+        // save list of bill detail
+        public static bool SaveBillDetailsBillAmount(BillStatus billStatus, IList<BillDetail> billDetails, BillAmount billAmount,List<DHFL_Liner> dhflLiners)
+        {
+            var session = SessionManager.GetCurrentSession();
+
+            using (var tx = session.BeginTransaction())
+            {
+                session.SaveOrUpdate(billStatus);
+
+                foreach (var entity in billDetails)
+                {
+                    session.SaveOrUpdate(entity);
+                }
+
+                foreach (var entity in dhflLiners)
+                {
+                    session.SaveOrUpdate(entity);
+                }
+
+                session.SaveOrUpdate(billAmount);
+
+                tx.Commit();
+            }
+
+            return true;
+        }
+
+    }
+}
