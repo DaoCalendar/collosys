2d0434863be391485c3d414c73a07e6d8609917d
Harish Rathi <harish.rathi@algosystech.com>
2014-06-02 16:28:16 +0530

    all bhattad scenario test cases added

---
+++ ColloSys.QueryBuilder.Test/Billing/BillingScenarioTests0302.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace ColloSys.QueryBuilder.Test.Billing
+{
+    [TestFixture]
+    public class BillingScenarioTests0302 : BillingScenarioTestBase
+    {
+        [TestCase("011762")]
+        [TestCase("003419")]
+        [TestCase("004447")]
+        public void _03_02_Reversal_Cases(string agentId)
+        {
+            RunBillingForBillStatus(201403, agentId, 201402);
+        }
+
+    }
+}
