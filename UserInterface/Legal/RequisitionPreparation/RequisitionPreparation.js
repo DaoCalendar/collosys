400e52d5c8c9c7b2d985b34ad7c11e8b4e42bc8e
Kapil Gandhi <kapilgandhi@algosys.com>
2014-06-06 19:22:18 +0530

    change excel-sheet to ui

--- UserInterface/Legal/RequisitionPreparation/RequisitionPreparation.js
+++ UserInterface/Legal/RequisitionPreparation/RequisitionPreparation.js
@@ -1,8 +1,42 @@
-﻿csapp.controller("RequisitionCtrl", ["$scope", "$csModels",
-    function ($scope,$csModels) {
+﻿csapp.controller("RequisitionCtrl", ["$scope", "$csModels", "RequisitionDataLayer",
+    function ($scope, $csModels, datalayer) {
         (function () {
+            $scope.datalayer = datalayer;
+            $scope.dldata = datalayer.dldata;
             $scope.legal = {};
-            $scope.legalpre = $csModels.getColumns("RequisitionPreparation");
+            $scope.legalprepare = $csModels.getColumns("RequisitionPreparation");
         })();
-    
+
+        $scope.Clear = function (legal) {
+            return datalayer.clear(legal);
+        };
+
     }]);
+
+csapp.factory("RequisitionDataLayer", ["$csnotify", function ($csnotify) {
+
+    var dldata = {};
+    var errorDisplay = function (response) {
+        $csnotify.error(response);
+    };
+    
+    dldata.Products = ['AUTO','SMC','SME-BIL','MORT'];
+    dldata.City = ['Mumbai', 'Pune', 'Kolkata', 'Bangalore', 'Hyderabad', 'Jaipur'];
+    dldata.loanstatus = ['Disbursed', 'Do/Sanction LetterMade', 'FCI Allocated', 'FCI Despatched', 'FCI Notrequired', 'Received','In-Principally Sanctioned'];
+    dldata.casestatus = ['Cancelled', 'Closed', 'Disposed', 'Expired', 'Legal', 'Live', 'Released','Repossessed'];
+
+    var clear = function(legal) {
+        legal.City = '';
+        legal.Products = '';
+        legal.LoanNo = '';
+        legal.LoanStatus = '';
+        legal.CaseStatus = '';
+    };
+
+
+    return {
+        dldata: dldata,
+        clear: clear,
+        //reset: resetdata
+    };
+}]);
