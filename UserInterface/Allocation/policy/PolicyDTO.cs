0ffd8cb178c267f0c466edd71600596cc55b3db9
Sonu Autade <sonuautade@algosystech.com>
2014-06-10 11:25:35 +0530

    CLSN-425  allocation policy screen similar to billing policy

---
+++ UserInterface/Allocation/policy/PolicyDTO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ColloSys.DataLayer.Allocation;
+using ColloSys.DataLayer.Enumerations;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace AngularUI.Allocation.policy
+{
+    public class PolicyDTO
+    {
+        public ScbEnums.Products Product;
+        public Guid PolicyId;
+        public List<SubpolicyDTO> IsInUseSubpolices = new List<SubpolicyDTO>();
+        public List<SubpolicyDTO> NotInUseSubpolices = new List<SubpolicyDTO>();
+
+        public void SetPolicyId(ISession session)
+        {
+            var allocPolicy = session.Query<AllocPolicy>()
+                .SingleOrDefault(x => x.Products == Product);
+
+            if (allocPolicy != null)
+            {
+                PolicyId = allocPolicy.Id;
+                return;
+            }
+
+            allocPolicy = new AllocPolicy
+            {
+                Name = "SystemCreated",
+                Products = Product
+            };
+
+            using (var tx = session.BeginTransaction())
+            {
+                session.SaveOrUpdate(allocPolicy);
+                tx.Commit();
+            }
+            PolicyId = allocPolicy.Id;
+        }
+    }
+
+}
---
+++ UserInterface/Allocation/policy/SubpolicyDTO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AngularUI.Billing.policy;
+using ColloSys.DataLayer.Allocation;
+using ColloSys.DataLayer.Enumerations;
+
+namespace AngularUI.Allocation.policy
+{
+    public class SubpolicyDTO
+    {
+        public string Name;
+        public uint Priority;
+        public ColloSysEnums.ApproveStatus ApproveStatus;
+        public DateTime StartDate;
+        public DateTime? EndDate;
+        public SubpolicyActivityEnum Activity;
+        public Guid PolicyId;
+        public Guid SubpolicyId;
+        public Guid RelationId;
+        public SubpolicyTypeEnum SubpolicyType;
+
+
+        public void Update(AllocRelation relation)
+        {
+            if (relation == null) return;
+            Priority = relation.Priority;
+            ApproveStatus = relation.Status;
+            StartDate = relation.StartDate;
+            EndDate = relation.EndDate;
+            RelationId = relation.Id;
+        }
+        public void Update(AllocSubpolicy subpolicy)
+        {
+            if (subpolicy == null) return;
+            Name = subpolicy.Name;
+            SubpolicyId = subpolicy.Id;
+        }
+    }
+
+    
+  
+}
