9e6a3958cd3bbe93f8222dcfb65e203d417767ae
Mahendra Pawar <mahendra.pawar@algosystech.com>
2014-06-16 16:53:05 +0530

    CLSN-472 TDD_FileUpload_E_WRITEOFF_AUTO files added for record creator and file reader

---
+++ FileUploader/AliasWriteOff/Ebbs/EWriteOffAutoFR.cs
@@ -0,0 +1,18 @@
+﻿#region references
+
+using ColloSys.DataLayer.Domain;
+using ColloSys.FileUploaderService.RecordManager;
+
+#endregion
+
+namespace ColloSys.FileUploaderService.AliasWriteOff.Ebbs
+{
+// ReSharper disable once InconsistentNaming
+    public class EWriteOffAutoFR : EWriteOffSharedFR
+    {
+        public EWriteOffAutoFR(FileScheduler fileScheduler, IRecord<EWriteoff> recordCreator)
+            : base(fileScheduler, recordCreator)
+        {
+        }
+    }
+}
