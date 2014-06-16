0bad15df0078367e49f35a41c2c8e37d7c643bfe
Mahendra Pawar <mahendra.pawar@algosystech.com>
2014-06-16 16:49:44 +0530

    CLSN-473 TDD_FileUpload_E_WRITEOFF_SMC files added for record creator and file reader

---
+++ FileUploader/AliasWriteOff/Ebbs/EWriteOffSharedFR.cs
@@ -0,0 +1,19 @@
+﻿#region references
+
+using ColloSys.DataLayer.Domain;
+using ColloSys.FileUploaderService.FileReader;
+using ColloSys.FileUploaderService.RecordManager;
+
+#endregion
+
+namespace ColloSys.FileUploaderService.AliasWriteOff.Ebbs
+{
+// ReSharper disable once InconsistentNaming
+    public class EWriteOffSharedFR:FileReader<EWriteoff>
+    {
+        public EWriteOffSharedFR(FileScheduler fileScheduler, IRecord<EWriteoff> recordCreator) 
+            : base(fileScheduler, recordCreator)
+        {
+        }
+    }
+}
