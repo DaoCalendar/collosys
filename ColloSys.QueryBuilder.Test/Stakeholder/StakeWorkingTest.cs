4fd764f4f85cbdaba26c4414f874ba56e6b35ec7
Soham Nakhare <soham.nakhare@algosystech.com>
2014-06-16 19:46:06 +0530

    CLSN-332 Stakeholder Working/Payment : Whole UI need to be changed, as its showing lost of empty rows, big size input box , scattered buttons,table view

---
+++ ColloSys.QueryBuilder.Test/Stakeholder/StakeWorkingTest.cs
@@ -0,0 +1,42 @@
+﻿using AngularUI.Stakeholder.addedit.Working;
+using NUnit.Framework;
+
+namespace ColloSys.QueryBuilder.Test.Stakeholder
+{
+    [TestFixture]
+    class StakeWorkingTest
+    {
+        [Test]
+        public void GetPincodeData()
+        {
+            var workModel = new WorkingModel
+                {
+                    SelectedPincodeData = { Country = "India" },
+                    QueryFor = LocationLevels.State,
+                    DisplayManager =
+                    {
+                        ShowState = true,
+                        ShowDistrict = true
+                    }
+                };
+
+            var pincodeData = workModel.SetWorkingList(workModel);
+
+            workModel.SelectedPincodeData.State = "Maharashtra";
+            workModel.QueryFor = LocationLevels.District;
+            pincodeData = workModel.SetWorkingList(workModel);
+
+            workModel.SelectedPincodeData.District = "Pune";
+            workModel.QueryFor = LocationLevels.Area;
+            pincodeData = workModel.SetWorkingList(workModel);
+
+            workModel.SelectedPincodeData.State = "Assam";
+            workModel.QueryFor = LocationLevels.District;
+            pincodeData = workModel.SetWorkingList(workModel);
+
+            Assert.AreEqual(0, pincodeData.ListOfAreas.Count);
+            Assert.AreEqual(5, pincodeData.ListOfDistricts.Count);
+
+        }
+    }
+}
--- UserInterface/Stakeholder/addedit/Working/workingScript.js
+++ UserInterface/Stakeholder/addedit/Working/workingScript.js
@@ -25,6 +25,13 @@ csapp.factory("StakeWorkingDatalayer", ["$csnotify", "Restangular", function ($c
             });
     };
 
+    var getPincodeData = function (workingModel) {
+        return restApi.customPOST(workingModel, 'GetPincodeData')
+          .then(function (data) {
+              return data;
+          });
+    };
+
     var savePayment = function (paymentData) {
         restApi.customPOST(paymentData, "SavePayment");
     };
@@ -33,7 +40,8 @@ csapp.factory("StakeWorkingDatalayer", ["$csnotify", "Restangular", function ($c
         GetStakeholder: getWorkingData,
         GetPaymentDetails: getPaymentDetails,
         SavePayment: savePayment,
-        GetReportsTo: getReportsTo
+        GetReportsTo: getReportsTo,
+        GetPincodeData: getPincodeData
     };
 }]);
 
@@ -47,88 +55,169 @@ csapp.factory("StakeWorkingFactory", ["$csfactory", function ($csfactory) {
         return fixedPayObj;
     };
 
+    var getQueryFor = function (locLevel) {
+        switch (locLevel.toUpperCase()) {
+            case "COUNTRY":
+                return "State";
+            case "CLUSTER":
+                return "State";
+            case "STATE":
+                return "State";
+            case "DISTRICT":
+                return "State";
+            case "CITY":
+                return "State";
+            case "AREA":
+                return "State";
+            default:
+                throw "invalid location level";
+        }
+    };
+
+    var displayManager = {
+        showCountry: true,
+        showRegion: false,
+        showCluster: false,
+        showState: false,
+        showCity: false,
+        showArea: false,
+    };
+
+    var getDisplayManager = function (locLevel) {
+        switch (locLevel.toUpperCase()) {
+            case "COUNTRY":
+                return displayManager;
+            case "CLUSTER":
+                displayManager.showState = true;
+                displayManager.showCluster = true;
+                return displayManager;
+            case "STATE":
+                displayManager.showState = true;
+                displayManager.showState = true;
+                return displayManager;
+            case "DISTRICT":
+                displayManager.showState = true;
+                displayManager.showDistrict = true;
+                return displayManager;
+            case "CITY":
+                displayManager.showState = true;
+                displayManager.showCity = true;
+                return displayManager;
+            case "AREA":
+                displayManager.showState = true;
+                displayManager.showArea = true;
+                return displayManager;
+            default:
+                throw "invalid location level";
+        }
+
+    };
+
     return {
-        GetFixedPayObj: getFixedPayObj
+        GetFixedPayObj: getFixedPayObj,
+        GetQueryFor: getQueryFor,
+        GetDisplayManager: getDisplayManager
     };
 }]);
 
-csapp.controller("StakeWorkingCntrl", ["$scope", "$routeParams", "StakeWorkingDatalayer", "$csModels", "StakeWorkingFactory", function ($scope, $routeParams, datalayer, $csModels, factory) {
+csapp.controller("StakeWorkingCntrl", ["$scope", "$routeParams", "StakeWorkingDatalayer", "$csModels", "StakeWorkingFactory", "$csfactory",
+    function ($scope, $routeParams, datalayer, $csModels, factory, $csfactory) {
 
-    (function () {
-        datalayer.GetStakeholder($routeParams.stakeId).then(function (data) {
-            $scope.selectedHierarchy = data.Hierarchy;
-            $scope.currStakeholder = data;
-        }).then(function () {
-            //TODO: has fixed pay individual
-            datalayer.GetPaymentDetails().then(function (data) {
-                $scope.FixedPay = factory.GetFixedPayObj(data);
+        (function () {
+            datalayer.GetStakeholder($routeParams.stakeId).then(function (data) {
+                data.Hierarchy.LocationLevelArray = JSON.parse(data.Hierarchy.LocationLevel);
+                data.Hierarchy.LocationLevel = data.Hierarchy.LocationLevelArray[0];
+                $scope.selectedHierarchy = data.Hierarchy;
+                $scope.displayManager = factory.GetDisplayManager($scope.selectedHierarchy.LocationLevel);
+                $scope.currStakeholder = data;
+            }).then(function () {
+                //TODO: has fixed pay individual
+                datalayer.GetPaymentDetails().then(function (data) {
+                    $scope.FixedPay = factory.GetFixedPayObj(data);
+                });
             });
-        });
+            $scope.paymentModel = $csModels.getColumns("StkhPayment");
+            $scope.workingModel = $csModels.getColumns("StkhWorking");
+            $scope.Payment = {};
+            $scope.WorkingModel = {
+                SelectedPincodeData: {},
+                QueryFor: ""
+            };
+        })();
 
-        $scope.paymentModel = $csModels.getColumns("StkhPayment");
-        $scope.workingModel = $csModels.getColumns("StkhWorking");
-        $scope.Payment = {};
-    })();
+        //TODO: move to factory
+        $scope.TotalPayment = function (basic, hra, other) {
+            $scope.SalDetails = {};
 
-    //TODO: move to factory
-    $scope.TotalPayment = function (basic, hra, other) {
-        $scope.SalDetails = {};
-
-        if (angular.isUndefined(basic)) {
-            basic = 0;
-        }
-        if (angular.isUndefined(hra)) {
-            hra = 0;
-        }
-        if (angular.isUndefined(other)) {
-            other = 0;
-        }
-        if (basic !== 0) {
-            if ($scope.selectedHierarchy.HasFixedIndividual || true) {
-                $scope.SalDetails.pf = Number((basic) * ($scope.FixedPay.EmployeePF) / 100);
-                $scope.SalDetails.pf = Number($scope.SalDetails.pf.toFixed(2));
-
-                $scope.SalDetails.employerPf = Number((basic) * ($scope.FixedPay.EmployerPF) / 100);
-                $scope.SalDetails.employerPf = Number($scope.SalDetails.employerPf.toFixed(2));
-
-                $scope.SalDetails.totalNotEsic = Number((basic) + (hra) + (other));
-
-                $scope.SalDetails.esicEmployee = Number(($scope.SalDetails.totalNotEsic * ($scope.FixedPay.EmployeeESIC)) / 100);
-                $scope.SalDetails.esicEmployee = Number($scope.SalDetails.esicEmployee.toFixed(2));
-
-                $scope.SalDetails.esicEmployer = Number($scope.SalDetails.totalNotEsic * Number($scope.FixedPay.EmployerESIC) / 100);
-                $scope.SalDetails.esicEmployer = Number($scope.SalDetails.esicEmployer.toFixed(2));
-
-                $scope.SalDetails.ServiceCharge = $scope.FixedPay.ServiceCharge !== 0 ? Number((basic * $scope.FixedPay.ServiceCharge)) / 100 : 0;
-                $scope.SalDetails.ServiceCharge = Number($scope.SalDetails.ServiceCharge.toFixed(2));
-
-                $scope.SalDetails.serviceTax = Number(basic * ($scope.FixedPay.ServiceTax) / 100);
-                $scope.SalDetails.serviceTax = Number($scope.SalDetails.serviceTax.toFixed(2));
-
-                $scope.SalDetails.total =
-                    $scope.SalDetails.pf * 2 +
-                    $scope.SalDetails.totalNotEsic +
-                    $scope.SalDetails.esicEmployee +
-                    $scope.SalDetails.esicEmployer +
-                    $scope.SalDetails.ServiceCharge +
-                    $scope.SalDetails.serviceTax;
-                $scope.SalDetails.total = Number($scope.SalDetails.total.toFixed(2));
-            } else {
-                $scope.SalDetails.total = basic;
+            if (angular.isUndefined(basic)) {
+                basic = 0;
             }
-        }
-        return 0;
-    };
+            if (angular.isUndefined(hra)) {
+                hra = 0;
+            }
+            if (angular.isUndefined(other)) {
+                other = 0;
+            }
+            if (basic !== 0) {
+                if ($scope.selectedHierarchy.HasFixedIndividual || true) {
+                    $scope.SalDetails.pf = Number((basic) * ($scope.FixedPay.EmployeePF) / 100);
+                    $scope.SalDetails.pf = Number($scope.SalDetails.pf.toFixed(2));
 
-    $scope.getReportsTo = function (product) {
-        datalayer.GetReportsTo($scope.currStakeholder).then(function (reportsToList) {
-            $scope.reportsToList = reportsToList;
-        });
-    };
+                    $scope.SalDetails.employerPf = Number((basic) * ($scope.FixedPay.EmployerPF) / 100);
+                    $scope.SalDetails.employerPf = Number($scope.SalDetails.employerPf.toFixed(2));
 
-    $scope.savePayment = function (paymentData) {
-        paymentData.Stakeholder = $scope.currStakeholder;
-        datalayer.SavePayment(paymentData);
-    };
+                    $scope.SalDetails.totalNotEsic = Number((basic) + (hra) + (other));
 
-}]);
+                    $scope.SalDetails.esicEmployee = Number(($scope.SalDetails.totalNotEsic * ($scope.FixedPay.EmployeeESIC)) / 100);
+                    $scope.SalDetails.esicEmployee = Number($scope.SalDetails.esicEmployee.toFixed(2));
+
+                    $scope.SalDetails.esicEmployer = Number($scope.SalDetails.totalNotEsic * Number($scope.FixedPay.EmployerESIC) / 100);
+                    $scope.SalDetails.esicEmployer = Number($scope.SalDetails.esicEmployer.toFixed(2));
+
+                    $scope.SalDetails.ServiceCharge = $scope.FixedPay.ServiceCharge !== 0 ? Number((basic * $scope.FixedPay.ServiceCharge)) / 100 : 0;
+                    $scope.SalDetails.ServiceCharge = Number($scope.SalDetails.ServiceCharge.toFixed(2));
+
+                    $scope.SalDetails.serviceTax = Number(basic * ($scope.FixedPay.ServiceTax) / 100);
+                    $scope.SalDetails.serviceTax = Number($scope.SalDetails.serviceTax.toFixed(2));
+
+                    $scope.SalDetails.total =
+                        $scope.SalDetails.pf * 2 +
+                        $scope.SalDetails.totalNotEsic +
+                        $scope.SalDetails.esicEmployee +
+                        $scope.SalDetails.esicEmployer +
+                        $scope.SalDetails.ServiceCharge +
+                        $scope.SalDetails.serviceTax;
+                    $scope.SalDetails.total = Number($scope.SalDetails.total.toFixed(2));
+                } else {
+                    $scope.SalDetails.total = basic;
+                }
+            }
+            return 0;
+        };
+
+        $scope.getReportsTo = function (product) {
+            datalayer.GetReportsTo($scope.currStakeholder).then(function (reportsToList) {
+                $scope.reportsToList = reportsToList;
+            });
+        };
+
+        $scope.savePayment = function (paymentData) {
+            paymentData.Stakeholder = $scope.currStakeholder;
+            datalayer.SavePayment(paymentData);
+        };
+
+        $scope.getPincodeData = function (workingModel, queryFor) {
+            workingModel.QueryFor = $csfactory.isNullOrEmptyString(queryFor) ?
+                factory.GetQueryFor($scope.selectedHierarchy.LocationLevel) : queryFor;
+            console.log("display Manager: ", $scope.displayManager);
+            datalayer.GetPincodeData(workingModel).then(function (data) {
+                $scope.WorkingModel = data;
+            });
+        };
+
+        $scope.showDropdown = function (locLevel) {
+            if ($csfactory.isEmptyObject($scope.selectedHierarchy)) return false;
+            return $scope.selectedHierarchy.LocationLevel === locLevel;
+        };
+
+    }]);
