ad7b6ee61042f8b38afd798a99a1b7d8b69703c4
Abhijeet Patil <abhijeet.patil@algosystech.com>
2014-06-18 10:21:42 +0530

    resovled test project issues adding file provider

---
+++ FileUploaderTest/FileProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionExtension.Tests
+{
+   public  class FileProvider
+    {
+       public FileInfo FileInfoForDrillDown { get; set; }
+       public FileInfo FileInfoForWoAuto { get; set; }
+
+       public FileProvider()
+       {
+           string filePath = "./ExcelData/DrillDown_Txn_1.xls";
+           string filePathForWoAuto = "./ExcelData/AEB Auto Charge Off Base - 28.01.2014.xls";
+           FileInfoForDrillDown=new FileInfo(filePath);
+           FileInfoForWoAuto = new FileInfo(filePathForWoAuto);
+
+       }
+
+
+    }
+}
