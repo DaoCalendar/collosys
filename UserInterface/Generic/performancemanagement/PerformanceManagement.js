27b53f303df2b0c4210a6b5b28a5e28a24c7ff18
Sonu Autade <sonuautade@algosystech.com>
2014-06-24 15:46:39 +0530

    CLSN-549  Performance Management Screen

--- UserInterface/Generic/performancemanagement/PerformanceManagement.js
+++ UserInterface/Generic/performancemanagement/PerformanceManagement.js
@@ -1,7 +1,71 @@
-﻿csapp.controller("PerformanaceManagementCtrl", ["$scope", function ($scope) {
+﻿csapp.factory("PerformanceManagemnetDatalayer", ["Restangular", function (rest) {
 
-    (function() {
-        $scope.acccc = "hello";
+    var dldata = {};
+    var restapi = rest.all("");
+
+    var save = function(performanceMgt) {
+        return restapi.customPOST("Saveperformanace", performanceMgt);
+    };
+
+    return {
+        dldata: dldata,
+        Save:save
+    };
+}]);
+
+csapp.controller("PerformanaceManagementCtrl", ["$scope", "PerformanceManagemnetDatalayer", "$csShared", function ($scope, datalayer, $csShared) {
+
+    (function () {
+        $scope.PerformanceModel = {
+            Product: { type: "enum", label: "Product" },
+            Weightage: { type: "number", template: "percentage" },
+        };
+        $scope.PerformanceModel.Product.valueList = $csShared.enums.Products;
+        $scope.ParamList = [];
+        $scope.performanceMgt = {
+            PerformanceParamses: [],
+            Products: ""
+        };
+        $scope.PerformanceParam = {
+            Param:[],
+            Weightage: []
+        };
     })();
 
+
+    $scope.save = function (performanceParam) {
+        performanceParam.Param = $scope.ParamList;
+        for (var i = 0; i < performanceParam.Param.length; i++) {
+            var item = {
+                Param: performanceParam.Param[i],
+                Weightage: performanceParam.Weightage[i]
+            };
+            $scope.performanceMgt.PerformanceParamses.push(item);
+        }
+        return datalayer.Save($scope.performanceMgt).then(function (data) {
+            
+        });
+
+
+    };
+
+    $scope.setParams = function (product) {
+        switch (product) {
+            case 'AUTO':
+                $scope.ParamList = $csShared.enums.Param_Auto;
+                break;
+            case 'MORT':
+                $scope.ParamList = $csShared.enums.Param_MORT;
+                break;
+            case 'AUTO_OD':
+                $scope.ParamList = $csShared.enums.Param_Auto_OD;
+                break;
+            case 'CC':
+                $scope.ParamList = $csShared.enums.Param_CC;
+                break;
+            default:
+                $scope.ParamList = ["SME_ME_Param1", "SME_ME_Param2", "SME_ME_Param3", "SME_ME_Param4"];
+        }
+
+    };
 }]);
